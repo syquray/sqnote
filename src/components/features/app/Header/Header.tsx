e8009d34d7f75e31d3221c047a181cc15af99dea
pscore23 <rubik2357x@gmail.com>
2023-06-10 15:45:51 +0900

    test:

--- src/components/features/app/Header/Header.tsx
+++ src/components/features/app/Header/Header.tsx
@@ -15,12 +15,12 @@ export const Header = () => {
           <a className="select-none text-primary-1 text-2xl md:text-3xl font-bold">
             sqnote
             <p className="text-sm md:text-base text-left">
-                  - the code is in yourself.
+              - the code is in yourself.
             </p>
           </a>
         </Link>
 
-        <div className="hidden lg:flex">
+        <div className="hidden lg:flex justify-center">
           <NavBar />
         </div>
 
